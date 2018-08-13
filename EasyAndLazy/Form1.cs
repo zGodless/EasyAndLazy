3a4018f630e10ce5c8b29cd76ed399050c8066bc
zGodless <zyl@cnsb.top>
2018-08-13 17:38:45 +0800

    tijiao

---
+++ EasyAndLazy/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EasyAndLazy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitEvent();
+            Init();
+        }
+
+        #region 初始化
+        public void Init()
+        {
+            //注册热键
+            HotKey.RegisterHotKey(Handle, 100, HotKey.KeyModifiers.Alt, Keys.J);
+            HotKey.RegisterHotKey(Handle, 101, HotKey.KeyModifiers.Alt, Keys.K);
+            HotKey.RegisterHotKey(Handle, 102, HotKey.KeyModifiers.Alt, Keys.Q);
+            HotKey.RegisterHotKey(Handle, 103, HotKey.KeyModifiers.Alt, Keys.F);
+            HotKey.RegisterHotKey(Handle, 104, HotKey.KeyModifiers.Alt, Keys.H);
+            //窗体置顶层
+            SetWindowPos(this.Handle, -1, 0, 0, 0, 0, 1 | 2);
+//             var control = textEdit1.Controls[0] as TextBox;
+//             control.Multiline = true;
+//             control.WordWrap = true;
+
+            textWide = HowMuchWord() - 5;
+        }
+
+
+        public void InitEvent()
+        {
+            Load += Form1_Load;
+            btnClose.Click += BtnClose_Click;
+
+            //鼠标点击事件
+            simpleButton1.MouseDown += Form1_MouseDown;
+            simpleButton1.MouseMove += Form1_MouseMove;
+            simpleButton1.MouseUp += Form1_MouseUp;
+        }
+        
+        #endregion
+        #region 属性
+
+        public string[] StoryText { get; set; }   //放置当前文本
+        public StreamReader HoleReader { get; set; }    //文件流
+        public int CurIndex { get; set; }   //当前阅读行
+        private INIClass ini { get; set; }      //配置类
+        private string section { get; set; }    //当前配置项
+
+        //用于拖动窗口
+        bool beginMove = false;//初始化鼠标位置  
+        int currentXPosition;
+        int currentYPosition;
+
+        //记录当前文本框宽度
+        private int textWide { get; set; }
+        #endregion
+
+
+        #region 事件
+
+        /// <summary>
+        /// 加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Location = new Point(150, 1015);
+            var open = new OpenFileDialog{
+                Filter = @"Files (*.txt)|*.txt"
+            };
+            if (open.ShowDialog() == DialogResult.OK)   //选择文件并加载文本到内存
+            {
+                LoadText(open.FileName);
+            }
+            else
+            {
+                MessageBox.Show("打开失败");
+                return;
+            }
+            string inipath = Application.StartupPath + @"\indexConfig.ini";     //读取配置文件获取上次阅读行
+            ini = new INIClass(inipath);
+            section = Path.GetFileNameWithoutExtension(open.FileName);
+            if (!ini.ExistINIFile())    //不存在ini文件则新建并初始化
+            {
+                FileStream filest = new FileStream(inipath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                filest.Flush();
+                filest.Close();
+                ini.IniWriteValue(section, "Path", open.FileName);
+                ini.IniWriteValue(section, "ReadIndex", "1");
+            }
+            if (string.IsNullOrEmpty(ini.IniReadValue(section, "ReadIndex")))
+            {
+                ini.IniWriteValue(section, "Path", open.FileName);
+                ini.IniWriteValue(section, "ReadIndex", "1");
+            }
+            CurIndex = Convert.ToInt32(ini.IniReadValue(section, "ReadIndex"));     //获取上次阅读行数
+            if (CurIndex != 0)
+            {
+                textEdit1.EditValue = StoryText[CurIndex];
+            }
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            FormClose();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                currentXPosition = 0; //设置初始状态  
+                currentYPosition = 0;
+                beginMove = false;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (beginMove)
+            {
+                this.Left += MousePosition.X - currentXPosition;//根据鼠标x坐标确定窗体的左边坐标x  
+                this.Top += MousePosition.Y - currentYPosition;//根据鼠标的y坐标窗体的顶部，即Y坐标  
+                currentXPosition = MousePosition.X;
+                currentYPosition = MousePosition.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                beginMove = true;
+                currentXPosition = MousePosition.X;//鼠标的x坐标为当前窗体左上角x坐标  
+                currentYPosition = MousePosition.Y;//鼠标的y坐标为当前窗体左上角y坐标  
+            }
+        }
+        #endregion
+        #region 方法
+
+        /// <summary>
+        /// 加载文本
+        /// </summary>
+        /// <param name="filePath">加载路径</param>
+        private void LoadText(string filePath)
+        {
+            StoryText = new string[300000];
+            HoleReader = new StreamReader(filePath, Encoding.UTF8);     //读取文本
+            string line = "";
+            for (int i = 1; (line = HoleReader.ReadLine()) != null; i++)
+            {
+                if (line == "")
+                {
+                    i--;
+                    continue;
+                }
+                int j = 1;
+                if (line.Length > textWide) //换行
+                {
+                    int len = line.Length;
+                    StoryText[i] = line.Substring(0, textWide);
+                    while (len - j * textWide > 0)
+                    {
+                        int leftWord = len - j * textWide;
+                        if (leftWord > textWide)
+                        {
+                            StoryText[i + j] = line.Substring(j * textWide, textWide);
+                        }
+                        else
+                        {
+                            StoryText[i + j] = line.Substring(j * textWide, leftWord);
+                        }
+                        j++;
+                    }
+                    i += j - 1;
+                }
+                else
+                {
+                    StoryText[i] = line;
+                }
+            }
+        }
+
+        private void FormClose()
+        {
+            HotKey.UnregisterHotKey(Handle, 100);//卸载第1个快捷键
+            HotKey.UnregisterHotKey(Handle, 101); //卸载第2个快捷键 
+            HotKey.UnregisterHotKey(Handle, 102); //卸载第3个快捷键
+            HotKey.UnregisterHotKey(Handle, 103); //卸载第4个快捷键
+            HotKey.UnregisterHotKey(Handle, 104); //卸载第4个快捷键
+            //记录当前阅读行数
+            ini.IniWriteValue(section, "ReadIndex", CurIndex.ToString().Trim());
+            Close();
+        }
+        //重写WndProc()方法，通过监视系统消息，来调用过程
+        protected override void WndProc(ref Message m)//监视Windows消息
+        {
+            const int WM_HOTKEY = 0x0312;
+            //按快捷键 
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32()){
+                        case 100:    //按下的是ALTER+J
+                            //if(StoryText[CurIndex + 1] == null) break;
+                            CurIndex++;
+                            textEdit1.EditValue = StoryText[CurIndex];
+                            break;
+                        case 101:    //按下的是ALTER+K
+                            if (CurIndex != 0)
+                            {
+                                CurIndex--;
+                                textEdit1.EditValue = StoryText[CurIndex];
+                            }
+                            break;
+                        case 102:    //按下的是Alt+Q
+                            FormClose();
+                            break;
+                        case 103:    //按下的是Alt+F
+                            FormSearch form = new FormSearch();
+                            form.StoryText = StoryText.ToList();
+                            if (form.ShowDialog() == DialogResult.OK)
+                            {
+                                CurIndex = form.ChoosedIndex;
+                                textEdit1.EditValue = StoryText[CurIndex];}
+                            break;
+                        case 104:    //按下的是Alt+H
+
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        //计算当前文本框一行可以装多少个字
+        private int HowMuchWord()
+        {
+            Graphics graphics = CreateGraphics();
+
+            SizeF sizeF = graphics.MeasureString("文", new Font("宋体", 9));
+            float wordNum = textEdit1.Width / sizeF.Width;
+            return Convert.ToInt32(wordNum);
+        }
+        #endregion
+
+        class HotKey
+        {
+            //如果函数执行成功，返回值不为0。
+            //如果函数执行失败，返回值为0。要得到扩展错误信息，调用GetLastError。
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool RegisterHotKey(
+                IntPtr hWnd,                //要定义热键的窗口的句柄
+                int id,                     //定义热键ID（不能与其它ID重复）           
+                KeyModifiers fsModifiers,   //标识热键是否在按Alt、Ctrl、Shift、Windows等键时才会生效
+                Keys vk                     //定义热键的内容
+            );
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool UnregisterHotKey(
+                IntPtr hWnd,                //要取消热键的窗口的句柄
+                int id                      //要取消热键的ID
+            );
+
+            //定义了辅助键的名称（将数字转变为字符以便于记忆，也可去除此枚举而直接使用数值）
+            [Flags()]
+            public enum KeyModifiers
+            {
+                None = 0,
+                Alt = 1,
+                Ctrl = 2,
+                Shift = 4,
+                WindowsKey = 8
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+        /// <summary>
+        /// 得到当前活动的窗口
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern System.IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+    }
+}
---
+++ EasyAndLazy/FormSearch.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace EasyAndLazy
+{
+    partial class FormSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControl2 = new DevExpress.XtraLayout.LayoutControl();
+            this.gcSearch = new DevExpress.XtraGrid.GridControl();
+            this.gvSearch = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.textSearch = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.galleryDropDown1 = new DevExpress.XtraBars.Ribbon.GalleryDropDown(this.components);
+            this.lookUpEdit1 = new DevExpress.XtraEditors.LookUpEdit();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl2)).BeginInit();
+            this.layoutControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gcSearch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSearch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textSearch.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryDropDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Location = new System.Drawing.Point(252, 83);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(8, 8);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(20, 20);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControl2
+            // 
+            this.layoutControl2.Controls.Add(this.lookUpEdit1);
+            this.layoutControl2.Controls.Add(this.gcSearch);
+            this.layoutControl2.Controls.Add(this.textSearch);
+            this.layoutControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl2.Name = "layoutControl2";
+            this.layoutControl2.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(492, 45, 450, 400);
+            this.layoutControl2.Root = this.layoutControlGroup2;
+            this.layoutControl2.Size = new System.Drawing.Size(754, 300);
+            this.layoutControl2.TabIndex = 1;
+            this.layoutControl2.Text = "layoutControl2";
+            // 
+            // gcSearch
+            // 
+            this.gcSearch.Location = new System.Drawing.Point(2, 26);
+            this.gcSearch.MainView = this.gvSearch;
+            this.gcSearch.Name = "gcSearch";
+            this.gcSearch.Size = new System.Drawing.Size(750, 248);
+            this.gcSearch.TabIndex = 5;
+            this.gcSearch.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvSearch});
+            // 
+            // gvSearch
+            // 
+            this.gvSearch.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2});
+            this.gvSearch.GridControl = this.gcSearch;
+            this.gvSearch.Name = "gvSearch";
+            this.gvSearch.OptionsView.ShowColumnHeaders = false;
+            this.gvSearch.OptionsView.ShowGroupPanel = false;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.AppearanceCell.Options.UseTextOptions = true;
+            this.gridColumn1.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridColumn1.AppearanceHeader.Options.UseTextOptions = true;
+            this.gridColumn1.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridColumn1.Caption = "行号";
+            this.gridColumn1.FieldName = "Index";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.OptionsColumn.AllowEdit = false;
+            this.gridColumn1.OptionsColumn.ReadOnly = true;
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            this.gridColumn1.Width = 80;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.AppearanceCell.Options.UseTextOptions = true;
+            this.gridColumn2.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridColumn2.AppearanceHeader.Options.UseTextOptions = true;
+            this.gridColumn2.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridColumn2.Caption = "文本";
+            this.gridColumn2.FieldName = "TextString";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.OptionsColumn.AllowEdit = false;
+            this.gridColumn2.OptionsColumn.ReadOnly = true;
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            this.gridColumn2.Width = 512;
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(2, 2);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(750, 20);
+            this.textSearch.StyleController = this.layoutControl2;
+            this.textSearch.TabIndex = 4;
+            // 
+            // layoutControlGroup2
+            // 
+            this.layoutControlGroup2.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup2.GroupBordersVisible = false;
+            this.layoutControlGroup2.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem4});
+            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup2.Name = "Root";
+            this.layoutControlGroup2.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 0, 0);
+            this.layoutControlGroup2.Size = new System.Drawing.Size(754, 300);
+            this.layoutControlGroup2.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.textSearch;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(754, 24);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.gcSearch;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(754, 252);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // galleryDropDown1
+            // 
+            this.galleryDropDown1.Manager = null;
+            this.galleryDropDown1.Name = "galleryDropDown1";
+            // 
+            // lookUpEdit1
+            // 
+            this.lookUpEdit1.Location = new System.Drawing.Point(2, 278);
+            this.lookUpEdit1.Name = "lookUpEdit1";
+            this.lookUpEdit1.Properties.AutoSearchColumnIndex = 1;
+            this.lookUpEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit1.Properties.CaseSensitiveSearch = true;
+            this.lookUpEdit1.Properties.Columns.AddRange(new DevExpress.XtraEditors.Controls.LookUpColumnInfo[] {
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("Index", "行号"),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("TextString", 220, "文本")});
+            this.lookUpEdit1.Properties.DisplayMember = "TextString";
+            this.lookUpEdit1.Properties.ImmediatePopup = true;
+            this.lookUpEdit1.Properties.NullText = "";
+            this.lookUpEdit1.Properties.PopupFilterMode = DevExpress.XtraEditors.PopupFilterMode.Contains;
+            this.lookUpEdit1.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.Standard;
+            this.lookUpEdit1.Properties.ValueMember = "Index";
+            this.lookUpEdit1.Size = new System.Drawing.Size(750, 20);
+            this.lookUpEdit1.StyleController = this.layoutControl2;
+            this.lookUpEdit1.TabIndex = 7;
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.lookUpEdit1;
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 276);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(754, 24);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // FormSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 300);
+            this.Controls.Add(this.layoutControl2);
+            this.Controls.Add(this.layoutControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormSearch";
+            this.Text = "FormSearch";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl2)).EndInit();
+            this.layoutControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gcSearch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSearch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textSearch.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryDropDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControl layoutControl2;
+        private DevExpress.XtraGrid.GridControl gcSearch;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvSearch;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraEditors.TextEdit textSearch;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraBars.Ribbon.GalleryDropDown galleryDropDown1;
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+    }
+}
